a54a6ddc8d9ca6712924ca557afc18e7ce49e592
Booh-rm <stivenatis@udenar.edu.co>
2024-10-09 12:21:07 -0500

    Add TeamListView and PlayerListView to album/views.py

--- album/views.py
+++ album/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from album.models import Team
+from album.models import Player
 
 # Create your views here.
+class TeamListView(ListView):
+    model = Team
+    template_name = 'album/team_list.html'
+    # context_object_name = 'teams'
+
+class PlayerListView(ListView):
+    model = Player
+    template_name = 'album/player_list.html'
